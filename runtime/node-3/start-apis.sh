b3a307ac14d5f519e1170d28df402bbae32602bd
Yang <zxcykuaile@gmail.com>
2019-01-10 18:30:36 +0100

    Add bitcoin dashboard
    Fix typos in mongodb

--- runtime/node-3/start-apis.sh
+++ runtime/node-3/start-apis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bitcoind -addnode=10.7.0.12:12345 -addnode=10.7.0.11:12345 -addnode=10.7.0.14:12345 -daemon -rpcuser=rpc -rpcpassword=x -rpcport=10340 --datadir=/root/bitcoind-bims/ -port=12345 -walletbroadcast=1
-mongod --dbpath /root/start/mongostore --fork --logpath ./mongostore/mongo.log
+mongod --dbpath /root/start/mongostore --fork --logpath /root/start/mongostore/mongo.log
 java -jar /root/apiscerana/iam-apiscerana-1.0-SNAPSHOT-fat.jar
