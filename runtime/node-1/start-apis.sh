c8fdbb79796ec30fb1094ace9d68834b21b4222d
Yang <zxcykuaile@gmail.com>
2019-01-10 22:10:10 +0100

    Detach the bitcoin db

--- runtime/node-1/start-apis.sh
+++ runtime/node-1/start-apis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bitcoind -addnode=10.7.0.12:12345 -addnode=10.7.0.13:12345 -addnode=10.7.0.14:12345 -daemon -rpcuser=rpc -rpcpassword=x -rpcport=10340  --datadir=/root/bitcoind-bims/ -port=12345 -walletbroadcast=1 -txindex=1 -server=1 -rest=1 -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332
+bitcoind -addnode=10.7.0.12:12345 -addnode=10.7.0.13:12345 -addnode=10.7.0.14:12345 -daemon -rpcuser=rpc -rpcpassword=x -rpcport=10340  --datadir=/root/start/bitcoind-bims/ -port=12345 -walletbroadcast=1 -txindex=1 -server=1 -rest=1 -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332
 mongod --dbpath /root/start/mongostore --fork --logpath /root/start/mongostore/mongo.log
 nohup java -jar /root/apiscerana/iam-apiscerana-1.0-SNAPSHOT-fat.jar &
 cd /root/dashboard/
