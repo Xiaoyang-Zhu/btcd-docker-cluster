0ca205bfc5167749e1b945c0152575a0ba3de9c4
Yang <zxcykuaile@gmail.com>
2019-01-10 14:55:59 +0100

    Daemon Mode
    Add the daemon mode to bitcoind
    delete nohup, since --fork --logpath means run in daemon

--- runtime/node-1/start-apis.sh
+++ runtime/node-1/start-apis.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-nohup mongod --dbpath /root/start/mongostore --fork --logpath ./mongostore/mongo.log
+bitcoind -addnode=10.7.0.12:12345 -addnode=10.7.0.13:12345 -addnode=10.7.0.14:12345 -daemon -rpcuser=rpc -rpcpassword=x -rpcport=10340  --datadir=/root/bitcoind-bims/ -port=12345 -walletbroadcast=1 -txindex=1 -server=1 -rest=1 -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332
+mongod --dbpath /root/start/mongostore --fork --logpath ./mongostore/mongo.log
 java -jar /root/apiscerana/iam-apiscerana-1.0-SNAPSHOT-fat.jar
-bitcoind -addnode=10.7.0.12:12345 -addnode=10.7.0.13:12345 -addnode=10.7.0.14:12345 -rpcuser=rpc -rpcpassword=x -rpcport=10340  --datadir=/root/bitcoind-bims/ -port=12345 -walletbroadcast=1 -txindex=1 -server=1 -rest=1 -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332
-/root/dashboard/start-dashboard.sh
--- runtime/node-3/start-apis.sh
+++ runtime/node-3/start-apis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup mongod --dbpath /root/start/mongostore --fork --logpath ./mongostore/mongo.log
+bitcoind -addnode=10.7.0.12:12345 -addnode=10.7.0.11:12345 -addnode=10.7.0.14:12345 -daemon -rpcuser=rpc -rpcpassword=x -rpcport=10340 --datadir=/root/bitcoind-bims/ -port=12345 -walletbroadcast=1
+mongod --dbpath /root/start/mongostore --fork --logpath ./mongostore/mongo.log
 java -jar /root/apiscerana/iam-apiscerana-1.0-SNAPSHOT-fat.jar
-bitcoind -addnode=10.7.0.12:12345 -addnode=10.7.0.11:12345 -addnode=10.7.0.14:12345 -rpcuser=rpc -rpcpassword=x -rpcport=10340 --datadir=/root/bitcoind-bims/ -port=12345 -walletbroadcast=1
