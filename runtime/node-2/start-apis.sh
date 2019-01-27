01d298639b68fa2ef92ae7f4f4bd9d8a8e4065c3
Yang <zxcykuaile@gmail.com>
2019-01-27 16:54:45 +0100

    Changes with adding parent directory in Apiscerana

--- runtime/node-2/start-apis.sh
+++ runtime/node-2/start-apis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bitcoind -addnode=10.7.0.11:12345 -addnode=10.7.0.13:12345 -addnode=10.7.0.14:12345 -daemon -rpcuser=rpc -rpcpassword=x -rpcport=10340 --datadir=/root/start/bitcoind-bims/ -port=12345 -walletbroadcast=1
 mongod --dbpath /var/lib/mongodb --fork --logpath /var/lib/mongodb/mongo.log
-java -jar /root/apiscerana/iam-apiscerana-1.0-SNAPSHOT-fat.jar
+java -jar /root/apiscerana/apiscerana-microservice-iam-1.0-SNAPSHOT-fat
